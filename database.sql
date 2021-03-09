b3f7b3d61d5e29f711e0e01a7ef24d8f19a6e650
Bennet <bennetdevelope@gmail.com>
2021-03-09 21:36:16 +0100

    added database.sql

---
+++ database.sql
@@ -0,0 +1,82 @@
+/*
+SQLyog Community v13.1.5  (64 bit)
+MySQL - 10.1.29-MariaDB : Database - messenger
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`messenger` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `messenger`;
+
+/*Table structure for table `chats` */
+
+CREATE TABLE `chats` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user1_id` int(10) unsigned DEFAULT NULL,
+  `user2_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user1_id` (`user1_id`),
+  KEY `fk_user2_id` (`user2_id`),
+  CONSTRAINT `fk_user1_id` FOREIGN KEY (`user1_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_user2_id` FOREIGN KEY (`user2_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `files` */
+
+CREATE TABLE `files` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `data_from` longblob,
+  `data_to` longblob,
+  `name` text,
+  `type` text,
+  `owner_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_by_id` (`owner_id`),
+  CONSTRAINT `fk_by_id` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `messages` */
+
+CREATE TABLE `messages` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `message_from` text,
+  `message_to` text,
+  `from_id` int(10) unsigned DEFAULT NULL,
+  `sent_datetime` datetime DEFAULT CURRENT_TIMESTAMP,
+  `chat_id` int(10) unsigned DEFAULT NULL,
+  `file_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_chat_id` (`chat_id`),
+  KEY `fk_from_id` (`from_id`),
+  KEY `fk_file_id` (`file_id`),
+  CONSTRAINT `fk_chat_id` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`id`),
+  CONSTRAINT `fk_file_id` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`),
+  CONSTRAINT `fk_from_id` FOREIGN KEY (`from_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=267 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `users` */
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` text,
+  `chat_id` int(11) DEFAULT NULL,
+  `session_id` int(11) DEFAULT NULL,
+  `password` text,
+  `public` blob,
+  `secret` blob,
+  `encryption_key` text,
+  `encryption_salt` text,
+  `online` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
